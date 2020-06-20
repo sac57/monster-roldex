d5bebfaf2c0c6f1cbd77830ceaf29c4306fb9e9d
sac57 <misolanke57@gmail.com>
2020-06-21 00:48:14 +0530

    Updates

---
+++ precache-manifest.1f6ed7cd7738ba2efb0da93a71778098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d2772e6bbd27abaab26150bcc6b28e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de8765fad3835042a27c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3103ab1e3a9e55a70d11",
+    "url": "/static/js/2.6c832960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c832960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8765fad3835042a27c",
+    "url": "/static/js/main.96f7a063.chunk.js"
+  },
+  {
+    "revision": "321be40e4d0b3008f64a",
+    "url": "/static/js/runtime-main.c34bb27e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
